854c199ce4f93da8cd784e78e0f5815b8acb90fa
wahyurizky99 <55475674+wahyurizky99@users.noreply.github.com>
2019-10-02 10:48:56 +0700

    update navbar

--- index.php
+++ index.php
@@ -15,13 +15,36 @@
 </head>
 <body>
 
-	<nav class="navbar navbar-light bg-light">
-	  <a class="navbar-brand">Navbar</a>
-	  <form class="form-inline">
-	    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-	    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-	  </form>
-	</nav>
+	<section class="header">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#"><img src="img/logo.png" width="25px"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <i class="fa fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ml-auto text-right">
+                <li class="nav-item">
+                  <a class="nav-link active-home" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#service">Service</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#feature">Feature</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#explore">Explore</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#testimonial">Testimonial</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#footer">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+        </nav>
 	
 </body>
 </html>
